b38a59ca99eb8971987795ce60042439cd8acc9a
Kush-Saharan <kush.saharan29@gmail.com>
2025-06-23 21:24:24 +0530

    Product page now loads for each item shoing options for buying

---
+++ app/models/user_cart.py
@@ -0,0 +1,13 @@
+from app.main import db
+from flask_login import UserMixin
+from datetime import datetime
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, default=1)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship('User', backref='cart_items')
+    product = db.relationship('Product')
--- app/routes/product_routes.py
+++ app/routes/product_routes.py
@@ -2,6 +2,7 @@
 from flask import Blueprint, render_template,request
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from app.models.product import Product, db
+from app.models.user_cart import Cart
 
 product_bp = Blueprint('product', __name__)
 
@@ -9,7 +10,7 @@ product_bp = Blueprint('product', __name__)
 @login_required
 def view_products():
     all_products = Product.query.all()
-    return render_template('product.html', products=all_products)
+    return render_template('products.html', products=all_products)
 
 @product_bp.route('/search',methods=['GET'])
 @login_required
@@ -18,6 +19,27 @@ def search_products():
     products = Product.query.filter(Product.name.ilike(f"%{name}%")).all()
     return render_template('index.html',products=products)
 
+@product_bp.route('/product/<int:id>')
+@login_required
+def product_details(id):
+    product=Product.query.filter_by(id=id).first()
+    return render_template('product_details.html',product=product)
+
+@product_bp.route('/product/buy/<int:id>')
+@login_required
+def product_buy(id):
+    pass
+
+@product_bp.route('/product/cart/<int:id>')
+@login_required
+def product_cart(id):
+    product_id=id
+    user_id=current_user.id
+    product_current=Cart.query.filter_by(product_id=product_id,user_id=user_id).first()
+    if product_current:
+        quantity=product_current.quantity()+1
+        product=Cart(product_id=product_id,user_id=user_id,quantity=quantity)
+        message="Product added successfully"
 
 
 @product_bp.route('/')
